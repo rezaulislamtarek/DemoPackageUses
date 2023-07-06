f5cf308c3bc2d8a7481246033edea7b2e733429a
rezaulislamtarek <mrezaulislam.t@gmail.com>
2023-07-06 18:14:51 +0600

    Initial Commit

---
+++ DemoPackageUses/DemoPackageUsesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoPackageUsesApp.swift
+//  DemoPackageUses
+//
+//  Created by Rezaul Islam on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoPackageUsesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
